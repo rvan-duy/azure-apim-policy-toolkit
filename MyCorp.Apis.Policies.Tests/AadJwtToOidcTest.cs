d6d7671aae1c5243acf4d0b24ac30861a5457bce
rvan-duy <rvanduyneveldt@gmail.com>
2025-07-01 15:27:06 +0200

    Initial project setup with .gitignore, solution file, project files, and implementation of AadJwtToOidc policy along with corresponding unit tests.

---
+++ MyCorp.Apis.Policies.Tests/AadJwtToOidcTest.cs
@@ -0,0 +1,194 @@
+﻿using MyCorp.Apis.Policies;
+
+using Azure.ApiManagement.PolicyToolkit.Testing.Expressions;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json.Linq;
+
+namespace MyCorp.Apis.Policies.Tests;
+
+[TestClass]
+public class AadJwtToOidcTest
+{
+    #region Decision Logic Tests
+
+    [TestMethod]
+    public void Should_Use_Cached_Token_When_Valid()
+    {
+        var context = new MockExpressionContext();
+
+        context.Variables["successCachedToken"] = "very-secret-token";
+
+        Assert.IsTrue(AadJwtToOidc.IsCachedTokenValid(context));
+    }
+
+    [TestMethod]
+    public void Should_Use_Scope_When_Cached_Token_Is_Invalid_And_Scope_Is_Set()
+    {
+        var context = new MockExpressionContext();
+
+        context.Variables["oidcScope"] = "test-scope";
+        context.Variables["oidcClientId"] = "test-client-id";
+        context.Variables["oidcClientSecret"] = "test-client-secret";
+        context.Variables["oidcGrantType"] = "client_credentials";
+        context.Variables["oidcUrl"] = "https://test.oidc.com";
+
+        Assert.IsTrue(AadJwtToOidc.IsScopeSet(context));
+        Assert.IsFalse(AadJwtToOidc.IsCachedTokenValid(context));
+        Assert.IsFalse(AadJwtToOidc.IsRefreshTokenSet(context));
+    }
+
+    [TestMethod]
+    public void Should_Use_Refresh_Token_When_Cached_Token_Is_Invalid_And_Refresh_Token_Is_Set()
+    {
+        var context = new MockExpressionContext();
+        
+        context.Variables["oidcRefreshToken"] = "test-refresh-token";
+        context.Variables["oidcClientId"] = "test-client-id";
+        context.Variables["oidcClientSecret"] = "test-client-secret";
+        context.Variables["oidcGrantType"] = "client_credentials";
+        context.Variables["oidcUrl"] = "https://test.oidc.com";
+
+        Assert.IsTrue(AadJwtToOidc.IsRefreshTokenSet(context));
+        Assert.IsFalse(AadJwtToOidc.IsCachedTokenValid(context));
+        Assert.IsFalse(AadJwtToOidc.IsScopeSet(context));
+    }
+
+    [TestMethod]
+    public void Should_Use_Other_Token_When_Cached_Token_Is_Invalid_And_Defaults_To_Other_Token()
+    {
+        var context = new MockExpressionContext();
+        
+        context.Variables["oidcClientId"] = "test-client-id";
+        context.Variables["oidcClientSecret"] = "test-client-secret";
+        context.Variables["oidcGrantType"] = "client_credentials";
+        context.Variables["oidcUrl"] = "https://test.oidc.com";
+
+        Assert.IsTrue(AadJwtToOidc.IsOtherTokenSet(context));
+        Assert.IsFalse(AadJwtToOidc.IsCachedTokenValid(context));
+        Assert.IsFalse(AadJwtToOidc.IsScopeSet(context));
+        Assert.IsFalse(AadJwtToOidc.IsRefreshTokenSet(context));
+    }
+
+    [TestMethod]
+    public void Should_Return_False_When_Cached_Token_Is_Invalid_And_No_Other_Token_Is_Set()
+    {
+        var context = new MockExpressionContext();
+
+        context.Variables["oidcClientId"] = "test-client-id";
+        context.Variables["oidcUrl"] = "https://test.oidc.com";
+
+        Assert.IsFalse(AadJwtToOidc.IsCachedTokenValid(context));
+        Assert.IsFalse(AadJwtToOidc.IsScopeSet(context));
+        Assert.IsFalse(AadJwtToOidc.IsRefreshTokenSet(context));
+        Assert.IsFalse(AadJwtToOidc.IsOtherTokenSet(context));
+    }
+
+    [TestMethod]
+    public void Should_Not_Use_Cached_Token_When_Cached_Token_Is_Invalid()
+    {
+        var context = new MockExpressionContext();
+        
+        context.Variables["cachedToken"] = "test-token";
+        context.Variables["cachedTokenExpiration"] = DateTime.UtcNow.AddHours(-1).ToString();
+
+        Assert.IsFalse(AadJwtToOidc.IsCachedTokenValid(context));
+        Assert.IsFalse(AadJwtToOidc.IsScopeSet(context));
+        Assert.IsFalse(AadJwtToOidc.IsRefreshTokenSet(context));
+        Assert.IsFalse(AadJwtToOidc.IsOtherTokenSet(context));
+    }
+
+    [TestMethod]
+    public void Should_Not_Use_Cached_Token_But_Use_Scope_When_Cached_Token_Is_Invalid_And_Scope_Is_Set()
+    {
+        var context = new MockExpressionContext();
+        
+        context.Variables["cachedToken"] = "test-token";
+        context.Variables["cachedTokenExpiration"] = DateTime.UtcNow.AddHours(-1).ToString();
+        context.Variables["oidcClientId"] = "test-client-id";
+        context.Variables["oidcClientSecret"] = "test-client-secret";
+        context.Variables["oidcGrantType"] = "client_credentials";
+        context.Variables["oidcUrl"] = "https://test.oidc.com";
+        context.Variables["oidcScope"] = "test-scope";
+
+        Assert.IsFalse(AadJwtToOidc.IsCachedTokenValid(context));
+        Assert.IsTrue(AadJwtToOidc.IsScopeSet(context));
+        Assert.IsFalse(AadJwtToOidc.IsRefreshTokenSet(context));
+    }
+
+    #endregion
+
+    #region Request Body Formatting Tests
+
+    [TestMethod]
+    public void Should_Format_Request_Body_For_Scope_Correctly()
+    {
+        var context = new MockExpressionContext();
+        
+        context.Variables["oidcClientId"] = "test-client-id";
+        context.Variables["oidcClientSecret"] = "test-client-secret";
+        context.Variables["oidcGrantType"] = "client_credentials";
+        context.Variables["oidcScope"] = "test-scope";
+
+        var expectedRequestBody = "client_id=test-client-id&client_secret=test-client-secret&grant_type=client_credentials&scope=test-scope";
+        var actualRequestBody = AadJwtToOidc.SetRequestBodyForScope(context);
+
+        Assert.AreEqual(expectedRequestBody, actualRequestBody);
+    }
+
+    [TestMethod]
+    public void Should_Format_Request_Body_For_Refresh_Token_Correctly()
+    {
+        var context = new MockExpressionContext();
+        
+        context.Variables["oidcClientId"] = "test-client-id";
+        context.Variables["oidcClientSecret"] = "test-client-secret";
+        context.Variables["oidcGrantType"] = "client_credentials";
+        context.Variables["oidcRefreshToken"] = "test-refresh-token";
+
+        var expectedRequestBody = "client_id=test-client-id&client_secret=test-client-secret&grant_type=client_credentials&refresh_token=test-refresh-token";
+        var actualRequestBody = AadJwtToOidc.SetRequestBodyForRefreshToken(context);
+
+        Assert.AreEqual(expectedRequestBody, actualRequestBody);
+    }
+
+    [TestMethod]
+    public void Should_Format_Request_Body_For_Other_Token_Correctly()
+    {
+        var context = new MockExpressionContext();
+        
+        context.Variables["oidcClientId"] = "test-client-id";
+        context.Variables["oidcClientSecret"] = "test-client-secret";
+        context.Variables["oidcGrantType"] = "client_credentials";
+
+        var expectedRequestBody = "client_id=test-client-id&client_secret=test-client-secret&grant_type=client_credentials";
+        var actualRequestBody = AadJwtToOidc.SetRequestBodyForOtherToken(context);
+
+        Assert.AreEqual(expectedRequestBody, actualRequestBody);
+    }
+
+    #endregion
+
+    #region Timeout Tests
+
+    [TestMethod]
+    public void Should_Use_Default_Policy_Timeout_When_Not_Set()
+    {
+        var context = new MockExpressionContext();
+
+        var timeout = AadJwtToOidc.GetPolicyTimeout(context);
+        Assert.AreEqual(30000, timeout);
+    }
+
+    [TestMethod]
+    public void Should_Use_Custom_Policy_Timeout_When_Set()
+    {
+        var context = new MockExpressionContext();
+        context.Variables["policy-timeout"] = 60000;
+
+        var timeout = AadJwtToOidc.GetPolicyTimeout(context);
+        Assert.AreEqual(60000, timeout);
+    }
+
+    #endregion
+}
---
+++ MyCorp.Apis.Policies/AadJwtToOidc.cs
@@ -0,0 +1,169 @@
+﻿using Azure.ApiManagement.PolicyToolkit.Authoring;
+using Azure.ApiManagement.PolicyToolkit.Authoring.Expressions;
+using Newtonsoft.Json.Linq;
+
+namespace MyCorp.Apis.Policies;
+
+[Document]
+public class AadJwtToOidc : IDocument
+{
+    public void Inbound(IInboundContext context)
+    {
+        context.CacheLookupValue(
+            new CacheLookupValueConfig
+            {
+                Key = "cachedToken",
+                VariableName = "successCachedToken",
+                CachingType = "internal"
+            }
+        );
+
+        if(IsCachedTokenValid(context.ExpressionContext))
+        {
+            context.SetHeader("Authorization", GetCachedToken(context.ExpressionContext));
+        }
+        else if (IsScopeSet(context.ExpressionContext))
+        {
+            context.SetVariable("requestBody", SetRequestBodyForScope(context.ExpressionContext));
+        }
+        else if (IsRefreshTokenSet(context.ExpressionContext))
+        {
+            context.SetVariable("requestBody", SetRequestBodyForRefreshToken(context.ExpressionContext));
+        }
+        else if (IsOtherTokenSet(context.ExpressionContext))
+        {
+            context.SetVariable("requestBody", SetRequestBodyForOtherToken(context.ExpressionContext));
+        }
+        else
+        {
+            context.InlinePolicy(
+                "<trace source=\"trace-name\" severity=\"error\"> <message>Missing variables in policy</message> </trace>"
+            );
+            context.ReturnResponse(
+                new ReturnResponseConfig
+                {
+                    Status = new StatusConfig
+                    {
+                        Code = 500,
+                        Reason = "Internal Server error"
+                    },
+                    Body = new BodyConfig
+                    {
+                        Content = "Internal Server error, please contact your admin"
+                    }
+                }
+            );
+        }
+
+        context.SendRequest(
+            new SendRequestConfig
+            {
+                ResponseVariableName = "accessTokenResponse",
+                Mode = "new",
+                Timeout = GetPolicyTimeout(context.ExpressionContext),
+                IgnoreError = false,
+                Url = "@((string)context.Variables[\"oidcUrl\"])",
+                Method = "POST",
+                Headers = new[] {
+                    new HeaderConfig
+                    {
+                        Name = "Content-Type",
+                        ExistsAction = "override",
+                        Values = new[] { "application/x-www-form-urlencoded" }
+                    }
+                },
+                Body = new BodyConfig
+                {
+                    Content = "@((string)context.Variables[\"requestBody\"])"
+                }
+            }
+        );
+
+        if (IsAccessTokenResponseSuccessful(context.ExpressionContext))
+        {
+            context.SetVariable("accessTokenResponseBody", GetAccessTokenResponseBody(context.ExpressionContext));
+            context.SetVariable("bearerToken", GetBearerToken(context.ExpressionContext));
+            context.CacheStoreValue(
+                new CacheStoreValueConfig
+                {
+                    Key = "cachedToken",
+                    Value = GetBearerToken(context.ExpressionContext),
+                    Duration = 3600,
+                    CachingType = "internal"
+                }
+            );
+            context.SetHeader("Authorization", GetAuthorizationHeader(context.ExpressionContext));
+        }
+        else
+        {
+            context.InlinePolicy("<trace source=\"trace-name\" severity=\"error\"> <message>TODO</message> </trace>" );
+            context.ReturnResponse(
+                new ReturnResponseConfig
+                {
+                    Status = new StatusConfig { Code = 401, Reason = "Unauthorized" },
+                    Body = new BodyConfig
+                    {
+                        Content = "@(((IResponse)context.Variables[\"accessTokenResponse\"]).Body.As<JObject>(preserveContent: true).ToString())"
+                    }
+                }
+            );
+        }
+    }
+
+    public static bool IsCachedTokenValid(IExpressionContext context)
+        => context.Variables.ContainsKey("successCachedToken");
+
+    public static string GetCachedToken(IExpressionContext context)
+        => "Bearer " + (string)context.Variables["cachedToken"];
+
+    public static bool IsScopeSet(IExpressionContext context)
+        => context.Variables.ContainsKey("oidcClientId") &&
+           context.Variables.ContainsKey("oidcClientSecret") &&
+           context.Variables.ContainsKey("oidcGrantType") &&
+           context.Variables.ContainsKey("oidcScope");
+
+    public static string SetRequestBodyForScope(IExpressionContext context)
+        => "client_id=" + (string)context.Variables["oidcClientId"] +
+           "&client_secret=" + (string)context.Variables["oidcClientSecret"] +
+           "&grant_type=" + (string)context.Variables["oidcGrantType"] +
+           "&scope=" + (string)context.Variables["oidcScope"];
+
+    public static bool IsRefreshTokenSet(IExpressionContext context)
+        => context.Variables.ContainsKey("oidcClientId") &&
+           context.Variables.ContainsKey("oidcClientSecret") &&
+           context.Variables.ContainsKey("oidcGrantType") &&
+           context.Variables.ContainsKey("oidcRefreshToken");
+
+    public static string SetRequestBodyForRefreshToken(IExpressionContext context)
+        => "client_id=" + (string)context.Variables["oidcClientId"] +
+           "&client_secret=" + (string)context.Variables["oidcClientSecret"] +
+           "&grant_type=" + (string)context.Variables["oidcGrantType"] +
+           "&refresh_token=" + (string)context.Variables["oidcRefreshToken"];
+
+    public static bool IsOtherTokenSet(IExpressionContext context)
+        => context.Variables.ContainsKey("oidcClientId") &&
+           context.Variables.ContainsKey("oidcClientSecret") &&
+           context.Variables.ContainsKey("oidcGrantType");
+
+    public static string SetRequestBodyForOtherToken(IExpressionContext context)
+        => "client_id=" + (string)context.Variables["oidcClientId"] +
+           "&client_secret=" + (string)context.Variables["oidcClientSecret"] +
+           "&grant_type=" + (string)context.Variables["oidcGrantType"];
+
+    public static bool IsAccessTokenResponseSuccessful(IExpressionContext context)
+        => ((IResponse)context.Variables["accessTokenResponse"]).StatusCode == 200;
+
+    public static JObject GetAccessTokenResponseBody(IExpressionContext context)
+        => ((IResponse)context.Variables["accessTokenResponse"]).Body.As<JObject>();
+
+    public static string GetBearerToken(IExpressionContext context)
+        => (string)((JObject)context.Variables["accessTokenResponseBody"])["access_token"];
+
+    public static string GetAuthorizationHeader(IExpressionContext context)
+        => "Bearer " + (string)context.Variables["bearerToken"];
+
+    public static int GetPolicyTimeout(IExpressionContext context)
+        => context.Variables.ContainsKey("policy-timeout") 
+            ? Convert.ToInt32(context.Variables["policy-timeout"]) 
+            : 30000;
+}
